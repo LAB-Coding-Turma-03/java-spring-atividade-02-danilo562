c8e976825a3b9005df98bb7335c69965ed17516d
Danilo Novaes <danilo.novaes@hotmail.com.br>
2021-05-15 11:55:19 -0300

    Update HelloWorldController.java
    feito a alteração proposta

--- src/main/java/br/com/impacta/lab/controller/HelloWorldController.java
+++ src/main/java/br/com/impacta/lab/controller/HelloWorldController.java
@@ -19,9 +19,10 @@ public class HelloWorldController {
 		//Considerando a dica acima, implemente seu código para que o programa retorne a 
 		//palavra Hello world concatenada com o nome.
 		//Exmeplo de resposta: Hello world felipe !
+		String ret = nome;
 		
 		
-		return ResponseEntity.ok("Hello world !");
+		return ResponseEntity.ok("Hello world ! "+ret);
 	}
 	
 }
